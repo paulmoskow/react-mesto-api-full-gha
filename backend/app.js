09d19a7267af54371251ab08aa7c83783f98004c
Pavel Moskov <pavel.moskov@gmail.com>
2024-01-22 17:05:35 +0100

    15 sprint

--- backend/app.js
+++ backend/app.js
@@ -40,15 +40,14 @@ app.get('/crash-test', () => {
 app.use(function(req, res, next) {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_METHODS);
   }
   next();
 });
