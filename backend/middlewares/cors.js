6ca924ac8d2e711be63ed04e0db78cabc163ca39
Pavel Moskov <pavel.moskov@gmail.com>
2024-01-28 02:37:45 +0100

    15 sprint correct ports logs cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,15 +10,14 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { origin, method } = req;
+  const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   res.header('Access-Control-Allow-Credentials', true);
 
   // temp allow for testing
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-  } else {
-    res.header('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
